c846be3e2210f7f4666e1d81b843cd8f0fdc86a8
dombeesley <quigley_beesley@hotmail.com>
2020-03-17 12:25:19 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello There!</h1>"
+
+
+if __name__ =="__main__":
+    app.run(host=os.getenv("IP", "0.0.0.0"),
+       port=int(os.getenv("PORT", "5000")),
+       debug=True)
+
